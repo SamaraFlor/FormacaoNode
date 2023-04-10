a63d8ee330ba79f37e65e14e3bf7aa94eebc84cf
Samara Flor <samaraflor2013>
2023-04-10 14:47:50 -0300

    adicionando callback

--- JavaScript/templateLiteral.js
+++ JavaScript/templateLiteral.js
@@ -1,7 +1,5 @@
 var nome = "Alice"
 var sobre = "Estudo na 5 serie da escola Vital Brasil"
 
-var frase = `          Ola eu sou ${nome} 
-
-                      e ${sobre}`;
+var frase = ` Ola eu sou ${nome} e  ${sobre}`;
 console.log(frase)
